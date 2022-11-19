10b5cb138ac04d544e3b3ca1de1a19e73d10a82b
Tomás Gómez Vecchio <tomasgomezvecchio@gmail.com>
2022-11-20 00:08:01 +0100

    update

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 'use strict';
 
-const switcher = document.querySelector('.btn');
+const RSS_URL = `https://www.democracynow.org/democracynow.rss`;
 
-switcher.addEventListener('click', function() {
-    document.body.classList.toggle('light-theme');
-    document.body.classList.toggle('dark-theme');
-
-    const className = document.body.className;
-    if(className == "light-theme") {
-        this.textContent = "Dark fux";
-    } else {
-        this.textContent = "Light";
-    }
-    console.log('current class name: ' + className);
-});
+fetch(RSS_URL)
+  .then(response => response.text())
+  .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+  .then(data => {
+    console.log(data);
+    const items = data.querySelectorAll("item");
+    let html = ``;
+    items.forEach(el => {
+      html += `
+        <article>
+          <img src="${el.querySelector("link").innerHTML}/image/large.png" alt="">
+          <h2>
+            <a href="${el.querySelector("link").innerHTML}" target="_blank" rel="noopener">
+              ${el.querySelector("title").innerHTML}
+            </a>
+          </h2>
+        </article>
+      `;
+    });
+    document.body.insertAdjacentHTML("beforeend", html);
+  });
